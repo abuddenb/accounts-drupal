40da292df0a658c7792f7c148ef14a81a7b2b334
abuddenberg <andrew.buddenberg@noaa.gov>
2016-04-05 16:14:48 -0400

    Add required version numbers; correct username.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# abuddenb:accounts-drupal
+# abuddenberg:accounts-drupal
 
 A Meteor login service for Drupal OAuth2 largely derived from [accounts-facebook](https://github.com/meteor/meteor/tree/devel/packages/accounts-facebook).
 Not at all affiliated with MDG and provided with no warranty.
 
-For more details, see the documentation pertaining to the [abuddenb:drupal](https://github.com/abuddenb/drupal) package.
+For more details, see the documentation pertaining to the [abuddenberg:drupal](https://github.com/abuddenb/drupal) package.
--- package.js
+++ package.js
@@ -2,16 +2,16 @@
  * Created by abuddenberg on 3/28/16.
  */
 Package.describe({
-    name: "abuddenb:accounts-drupal",
+    name: "abuddenberg:accounts-drupal",
     summary: "Login service for Drupal 7 accounts",
     version: "0.0.1"
 });
 
 Package.onUse(function(api) {
-    api.use('accounts-base', ['client', 'server']);
+    api.use('accounts-base@1.2.2', ['client', 'server']);
     api.imply('accounts-base', ['client', 'server']);
-    api.use('accounts-oauth', ['client', 'server']);
-    api.use('abuddenb:drupal', ['client', 'server']);
+    api.use('accounts-oauth@1.1.8', ['client', 'server']);
+    api.use('abuddenberg:drupal@0.0.1', ['client', 'server']);
 
     // api.addFiles('drupal_login_button.css', 'client');
 
